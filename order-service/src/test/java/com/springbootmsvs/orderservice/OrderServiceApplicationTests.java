796bfe6f4d41cdef3ba59c54c986cdabb03df502
naivedeveloper95 <findsatyam95@gmail.com>
2023-06-10 20:53:35 +0530

    Added order service

---
+++ order-service/src/test/java/com/springbootmsvs/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmsvs.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
