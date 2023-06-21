14ea146a11a7284e71862ac3d5c6d1d961af71fe
naivedeveloper95 <findsatyam95@gmail.com>
2023-06-22 00:38:13 +0530

    Added Distributed Tracing to microservices

--- order-service/src/main/java/com/springbootmsvs/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/springbootmsvs/orderservice/service/OrderService.java
@@ -7,6 +7,8 @@ import com.springbootmsvs.orderservice.model.Order;
 import com.springbootmsvs.orderservice.model.OrderLineItems;
 import com.springbootmsvs.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -22,6 +24,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -38,22 +41,28 @@ public class OrderService {
                  .stream()
                  .map(OrderLineItems::getSkuCode)
                  .toList();
-         InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
-                .uri(
-                        "http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
+        Span inventoryServiceLookup = tracer.nextSpan().name("inventoryServiceLookup");
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())) {
+            InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                    .uri(
+                            "http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponses)
-                .allMatch(InventoryResponse::isInStock);
+            boolean allProductsInStock = Arrays.stream(inventoryResponses)
+                    .allMatch(InventoryResponse::isInStock);
 
-        if (Boolean.TRUE.equals(allProductsInStock)) {
-            orderRepository.save(order);
-        } else {
-            throw new IllegalArgumentException("Product is not in stock, please try again later");
-        }
+            if (Boolean.TRUE.equals(allProductsInStock)) {
+                orderRepository.save(order);
+            } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later");
+            }
+        } finally {
+            inventoryServiceLookup.end();
+        };
+
         return "Order places successfully.";
     }
 
